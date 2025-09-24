33bffaedc0a751f7e860247b90872ea099bacf79
Taneli Keranen <taneli.keranen@glaston.net>
2025-09-24 16:27:50 +0300

    [review] let database assign the id for watch task

--- AiWebSiteWatchDog.Application/Services/WatcherService.cs
+++ AiWebSiteWatchDog.Application/Services/WatcherService.cs
@@ -13,8 +13,14 @@ namespace AiWebSiteWatchDog.Application.Services
         public async Task<WatchTask> CheckWebsiteAsync(UserSettings settings)
         {
             var result = await _geminiApiClient.CheckInterestAsync(settings.WatchUrl, settings.InterestSentence, settings.GeminiApiKey);
-            // If WatchTask should store senderEmail, update WatchTask to accept it, otherwise just remove EmailSettingsId
-            return new WatchTask(0, settings.WatchUrl, settings.InterestSentence, DateTime.UtcNow, result);
+            
+            return new WatchTask
+            {
+                Url = settings.WatchUrl,
+                InterestSentence = settings.InterestSentence,
+                LastChecked = DateTime.UtcNow,
+                LastResult = result
+            };
         }
     }
 }
--- AiWebSiteWatchDog.Domain/Entities/WatchTask.cs
+++ AiWebSiteWatchDog.Domain/Entities/WatchTask.cs
@@ -2,12 +2,31 @@ using System;
 
 namespace AiWebSiteWatchDog.Domain.Entities
 {
-    public class WatchTask(int id, string url, string interestSentence, DateTime lastChecked, string? lastResult)
+    public class WatchTask
     {
-        public int Id { get; set; } = id;
-        public string Url { get; set; } = url;
-        public string InterestSentence { get; set; } = interestSentence;
-        public DateTime LastChecked { get; set; } = lastChecked;
-        public string? LastResult { get; set; } = lastResult;
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public required string InterestSentence { get; set; }
+    public DateTime LastChecked { get; set; }
+    public string? LastResult { get; set; }
+
+        public WatchTask() { }
+
+        public WatchTask(string url, string interestSentence, DateTime lastChecked, string? lastResult)
+        {
+            Url = url;
+            InterestSentence = interestSentence;
+            LastChecked = lastChecked;
+            LastResult = lastResult;
+        }
+
+        public WatchTask(int id, string url, string interestSentence, DateTime lastChecked, string? lastResult)
+        {
+            Id = id;
+            Url = url;
+            InterestSentence = interestSentence;
+            LastChecked = lastChecked;
+            LastResult = lastResult;
+        }
     }
 }
