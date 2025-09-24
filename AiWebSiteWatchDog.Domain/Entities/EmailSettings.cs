0cffa069a1375f73ddb734d39e4440f0e9cb99a9
Taneli Keranen <taneli.keranen@glaston.net>
2025-09-24 15:39:52 +0300

    Modify domain models.

--- AiWebSiteWatchDog.Domain/Entities/EmailSettings.cs
+++ AiWebSiteWatchDog.Domain/Entities/EmailSettings.cs
@@ -1,13 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 namespace AiWebSiteWatchDog.Domain.Entities
 {
-    public class EmailSettings(int id, string smtpServer, int smtpPort, string senderEmail, string senderName, string appPassword, bool enableSsl)
+    public class EmailSettings(string senderEmail, string senderName, string gmailClientSecretJson)
     {
-        public int Id { get; set; } = id;
-        public string SmtpServer { get; set; } = smtpServer;
-        public int SmtpPort { get; set; } = smtpPort;
+        [Key]
         public string SenderEmail { get; set; } = senderEmail;
         public string SenderName { get; set; } = senderName;
-        public string AppPassword { get; set; } = appPassword;
-        public bool EnableSsl { get; set; } = enableSsl;
+        public string GmailClientSecretJson { get; set; } = gmailClientSecretJson;
     }
 }
